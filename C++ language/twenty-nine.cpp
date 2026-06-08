788b003a2057b225e1e4a68fb91b6f7ac9f657d8
baliyananmol-166 <baliyananmol166@gamil.com>
2026-06-08 20:09:41 +0530

    Pointer

---
+++ C++ language/twenty-nine.cpp
@@ -0,0 +1,12 @@
+//example of pointer
+#include <iostream>
+using namespace std;
+int main() {
+    int val = 105;
+    int *ptr;
+    ptr=&val;
+    cout<<"The value of val is :"<<val<<endl;
+    cout<<"The value of ptr is: "<<ptr<<endl;
+    cout<<"The value of *ptr is: "<<*ptr<<endl;
+    return 0;
+}
