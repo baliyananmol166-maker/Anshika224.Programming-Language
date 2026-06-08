308b367f242aab1d31bfdcf5093dd6d5c7d5d0f5
baliyananmol-166 <baliyananmol166@gamil.com>
2026-06-08 16:40:11 +0530

    Break/Continue

---
+++ C++ language/sixteen.cpp
@@ -0,0 +1,15 @@
+//example of continue statement in C++
+#include <iostream>
+using namespace std;
+int main() {
+    int num =100;
+    while(num>=90){
+        if(num==96){
+        num--;
+        continue;
+      }
+        cout<<"Value is :"<< num << endl;
+        num--;
+    }
+    return 0;
+}
